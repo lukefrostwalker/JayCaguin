0c1ddd6b8598dbd5c7c211f1600e1ff8c4588542
lukefrostwalker <116066162+lukefrostwalker@users.noreply.github.com>
2023-03-29 17:35:49 +0800

    modified: js files

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,96 +1,158 @@
+const _0x3378b3 = _0x2c96;
+(function (_0x3ff1a5, _0x1e9026) {
+    const _0x536e58 = _0x2c96,
+        _0x16929e = _0x3ff1a5();
+    while (!![]) {
+        try {
+            const _0x5675ca =
+                (parseInt(_0x536e58(0x201)) / 0x1) *
+                    (parseInt(_0x536e58(0x1ff)) / 0x2) +
+                parseInt(_0x536e58(0x21a)) / 0x3 +
+                (parseInt(_0x536e58(0x206)) / 0x4) *
+                    (parseInt(_0x536e58(0x1fc)) / 0x5) +
+                (parseInt(_0x536e58(0x205)) / 0x6) *
+                    (-parseInt(_0x536e58(0x220)) / 0x7) +
+                (-parseInt(_0x536e58(0x218)) / 0x8) *
+                    (-parseInt(_0x536e58(0x1f6)) / 0x9) +
+                parseInt(_0x536e58(0x20d)) / 0xa +
+                -parseInt(_0x536e58(0x21d)) / 0xb;
+            if (_0x5675ca === _0x1e9026) break;
+            else _0x16929e['push'](_0x16929e['shift']());
+        } catch (_0x3ee0d1) {
+            _0x16929e['push'](_0x16929e['shift']());
+        }
+    }
+})(_0x3438, 0xe6cd6);
+function _0x3438() {
+    const _0xcc73d4 = [
+        'Express.js',
+        '711396sRrahG',
+        'https://lukefrostwalker.github.io/Password-GeneratorV2/',
+        'https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg',
+        'https://lukefrostwalker.github.io/TodoListv2/',
+        'https://lukefrostwalker.github.io/Converter/',
+        'Unit\x20Converter',
+        '15BbOhvv',
+        'Node.js',
+        'https://github.com/lukefrostwalker/TodoListv2',
+        '1040990wFrJUP',
+        'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg',
+        '1Nlxlxv',
+        'https://github.com/lukefrostwalker/Password-GeneratorV2',
+        'https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg',
+        'HTML5',
+        '7645170fJylrr',
+        '2407620VTepPc',
+        'https://upload.wikimedia.org/wikipedia/commons/a/af/Adobe_Photoshop_CC_icon.svg',
+        'JavaScript',
+        'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+        'CodeFit\x20Meals',
+        'Length\x20Converter',
+        'https://github.com/lukefrostwalker/RockPaperScissorsLizardSpock',
+        '3245290UManmZ',
+        'Bootstrap',
+        'https://upload.wikimedia.org/wikipedia/commons/4/48/Adobe_InDesign_CC_icon.svg',
+        'UrbanRead\x20(MERN)',
+        'https://lukefrostwalker.github.io/RockPaperScissorsLizardSpock',
+        'https://github.com/lukefrostwalker/UnitConverter-Scrim-',
+        '1750',
+        'https://urbanread.space/',
+        'https://upload.wikimedia.org/wikipedia/commons/9/9a/Laravel.svg',
+        '1000',
+        'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
+        '160Vlumsh',
+        'https://github.com/lukefrostwalker/Converter',
+        '3895137cmFzQY',
+        'Laravel',
+        'https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg',
+        '36414807zkNLYm',
+        'CSS3',
+        'https://lukefrostwalker.github.io/CodeFitMeals/',
+        '7RvQLXy',
+        'Password\x20Generator',
+        'Photoshop',
+        'InDesign',
+    ];
+    _0x3438 = function () {
+        return _0xcc73d4;
+    };
+    return _0x3438();
+}
 const works = [
-    {
-        id: 1,
-        name: 'UrbanRead (MERN)',
-        repo: 'https://github.com/lukefrostwalker/UrbanRead-MP3',
-        url: 'https://urbanread.space/',
-        duration: '1000',
-    },
-    {
-        id: 2,
-        name: 'CodeFit Meals',
-        repo: 'https://github.com/lukefrostwalker/CodeFitMeals',
-        url: 'https://lukefrostwalker.github.io/CodeFitMeals/',
-        duration: '1750',
-    },
-    {
-        id: 3,
-        name: 'Jajanken+',
-        repo: 'https://github.com/lukefrostwalker/RockPaperScissorsLizardSpock',
-        url: 'https://lukefrostwalker.github.io/RockPaperScissorsLizardSpock',
-        duration: '1000',
-    },
-    {
-        id: 4,
-        name: 'Password Generator',
-        repo: 'https://github.com/lukefrostwalker/Password-GeneratorV2',
-        url: 'https://lukefrostwalker.github.io/Password-GeneratorV2/',
-        duration: '1750',
-    },
-    {
-        id: 5,
-        name: 'To-do List',
-        repo: 'https://github.com/lukefrostwalker/TodoListv2',
-        url: 'https://lukefrostwalker.github.io/TodoListv2/',
-        duration: '1000',
-    },
-    {
-        id: 6,
-        name: 'Length Converter',
-        repo: 'https://github.com/lukefrostwalker/Converter',
-        url: 'https://lukefrostwalker.github.io/Converter/',
-        duration: '1750',
-    },
-    {
-        id: 7,
-        name: 'Unit Converter',
-        repo: 'https://github.com/lukefrostwalker/UnitConverter-Scrim-',
-        url: 'https://lukefrostwalker.github.io/UnitConverter-Scrim-/',
-        duration: '1000',
-    },
-];
-
-const skills = [
-    {
-        title: 'HTML5',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg',
-    },
-    {
-        title: 'CSS3',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg',
-    },
-    {
-        title: 'Bootstrap',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg',
-    },
-    {
-        title: 'JavaScript',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
-    },
-    {
-        title: 'React',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
-    },
-    {
-        title: 'Laravel',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Laravel.svg',
-    },
-    {
-        title: 'Photoshop',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/a/af/Adobe_Photoshop_CC_icon.svg',
-    },
-    {
-        title: 'InDesign',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/4/48/Adobe_InDesign_CC_icon.svg',
-    },
-    {
-        title: 'Node.js',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg',
-    },
-    {
-        title: 'Express.js',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png',
-    },
-];
-
+        {
+            id: 0x1,
+            name: _0x3378b3(0x210),
+            repo: 'https://github.com/lukefrostwalker/UrbanRead-MP3',
+            url: _0x3378b3(0x214),
+            duration: _0x3378b3(0x216),
+        },
+        {
+            id: 0x2,
+            name: _0x3378b3(0x20a),
+            repo: 'https://github.com/lukefrostwalker/CodeFitMeals',
+            url: _0x3378b3(0x21f),
+            duration: '1750',
+        },
+        {
+            id: 0x3,
+            name: 'Jajanken+',
+            repo: _0x3378b3(0x20c),
+            url: _0x3378b3(0x211),
+            duration: _0x3378b3(0x216),
+        },
+        {
+            id: 0x4,
+            name: _0x3378b3(0x1f2),
+            repo: _0x3378b3(0x202),
+            url: _0x3378b3(0x1f7),
+            duration: _0x3378b3(0x213),
+        },
+        {
+            id: 0x5,
+            name: 'To-do\x20List',
+            repo: _0x3378b3(0x1fe),
+            url: _0x3378b3(0x1f9),
+            duration: '1000',
+        },
+        {
+            id: 0x6,
+            name: _0x3378b3(0x20b),
+            repo: _0x3378b3(0x219),
+            url: _0x3378b3(0x1fa),
+            duration: _0x3378b3(0x213),
+        },
+        {
+            id: 0x7,
+            name: _0x3378b3(0x1fb),
+            repo: _0x3378b3(0x212),
+            url: 'https://lukefrostwalker.github.io/UnitConverter-Scrim-/',
+            duration: _0x3378b3(0x216),
+        },
+    ],
+    skills = [
+        { title: _0x3378b3(0x204), src: _0x3378b3(0x203) },
+        { title: _0x3378b3(0x21e), src: _0x3378b3(0x1f8) },
+        { title: _0x3378b3(0x20e), src: _0x3378b3(0x21c) },
+        { title: _0x3378b3(0x208), src: _0x3378b3(0x217) },
+        { title: 'React', src: _0x3378b3(0x209) },
+        { title: _0x3378b3(0x21b), src: _0x3378b3(0x215) },
+        { title: _0x3378b3(0x1f3), src: _0x3378b3(0x207) },
+        { title: _0x3378b3(0x1f4), src: _0x3378b3(0x20f) },
+        { title: _0x3378b3(0x1fd), src: _0x3378b3(0x200) },
+        {
+            title: _0x3378b3(0x1f5),
+            src: 'https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png',
+        },
+    ];
+function _0x2c96(_0x30d11c, _0x4b21b3) {
+    const _0x3438bf = _0x3438();
+    return (
+        (_0x2c96 = function (_0x2c96bf, _0x11f677) {
+            _0x2c96bf = _0x2c96bf - 0x1f2;
+            let _0x1a23e0 = _0x3438bf[_0x2c96bf];
+            return _0x1a23e0;
+        }),
+        _0x2c96(_0x30d11c, _0x4b21b3)
+    );
+}
 export { works, skills };
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,79 @@
+const _0x29a3d9 = _0x3777;
+(function (_0x51acf1, _0x457590) {
+    const _0x28981f = _0x3777,
+        _0x36d0d1 = _0x51acf1();
+    while (!![]) {
+        try {
+            const _0x367640 =
+                -parseInt(_0x28981f(0x134)) / 0x1 +
+                -parseInt(_0x28981f(0x13a)) / 0x2 +
+                parseInt(_0x28981f(0x12e)) / 0x3 +
+                parseInt(_0x28981f(0x132)) / 0x4 +
+                (-parseInt(_0x28981f(0x13c)) / 0x5) *
+                    (-parseInt(_0x28981f(0x137)) / 0x6) +
+                parseInt(_0x28981f(0x12f)) / 0x7 +
+                (-parseInt(_0x28981f(0x135)) / 0x8) *
+                    (parseInt(_0x28981f(0x133)) / 0x9);
+            if (_0x367640 === _0x457590) break;
+            else _0x36d0d1['push'](_0x36d0d1['shift']());
+        } catch (_0x18f8df) {
+            _0x36d0d1['push'](_0x36d0d1['shift']());
+        }
+    }
+})(_0x5397, 0xeccfe);
 import {
     renderFooter,
     renderSkills,
     renderNav,
     renderWorks,
 } from './render.js';
-const currentUrl = window.location.href;
-
-AOS.init();
-
-renderNav();
-
-if (
-    currentUrl.indexOf('works') > -1 ||
-    currentUrl.indexOf('about') > -1 ||
-    currentUrl.indexOf('contact') > -1 ||
-    currentUrl.indexOf('thank-you') > -1
-) {
+const currentUrl = window[_0x29a3d9(0x12d)][_0x29a3d9(0x130)];
+AOS['init'](), renderNav();
+(currentUrl[_0x29a3d9(0x138)](_0x29a3d9(0x139)) > -0x1 ||
+    currentUrl[_0x29a3d9(0x138)](_0x29a3d9(0x13b)) > -0x1 ||
+    currentUrl[_0x29a3d9(0x138)]('contact') > -0x1 ||
+    currentUrl['indexOf']('thank-you') > -0x1) &&
     renderFooter();
+function _0x5397() {
+    const _0x4e1bc9 = [
+        'works',
+        '1351018OnihyA',
+        'about',
+        '9612145vDyitZ',
+        'location',
+        '4808181VqTliL',
+        '13542487yvQqvb',
+        'href',
+        'Tooltip',
+        '613028RbNyRl',
+        '4545DhOdyP',
+        '1415798qFqQSu',
+        '40424FPEeHz',
+        'map',
+        '6fqsXob',
+        'indexOf',
+    ];
+    _0x5397 = function () {
+        return _0x4e1bc9;
+    };
+    return _0x5397();
 }
-
-if (currentUrl.indexOf('works') > -1) {
-    renderWorks();
-} else if (currentUrl.indexOf('about') > -1) {
-    renderSkills();
+function _0x3777(_0x265797, _0x5a4d2e) {
+    const _0x539753 = _0x5397();
+    return (
+        (_0x3777 = function (_0x377718, _0x1bb30a) {
+            _0x377718 = _0x377718 - 0x12d;
+            let _0x2aabcd = _0x539753[_0x377718];
+            return _0x2aabcd;
+        }),
+        _0x3777(_0x265797, _0x5a4d2e)
+    );
 }
-
-const tooltipTriggerList = document.querySelectorAll(
-    '[data-bs-toggle="tooltip"]'
-);
-const tooltipList = [...tooltipTriggerList].map(
-    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
-);
+if (currentUrl[_0x29a3d9(0x138)](_0x29a3d9(0x139)) > -0x1) renderWorks();
+else currentUrl[_0x29a3d9(0x138)](_0x29a3d9(0x13b)) > -0x1 && renderSkills();
+const tooltipTriggerList = document['querySelectorAll'](
+        '[data-bs-toggle=\x22tooltip\x22]'
+    ),
+    tooltipList = [...tooltipTriggerList][_0x29a3d9(0x136)](
+        (_0x486b78) => new bootstrap[_0x29a3d9(0x131)](_0x486b78)
+    );
